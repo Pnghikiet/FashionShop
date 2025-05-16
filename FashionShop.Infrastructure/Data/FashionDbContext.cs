825a70f1b88f086463f804f7698a338cb5abba74
Pnghikiet <phungnghikiet@gmail.com>
2025-05-16 11:07:04 +0700

    Add project files.

---
+++ FashionShop.Infrastructure/Data/FashionDbContext.cs
@@ -0,0 +1,26 @@
+﻿using FashionShop.Core.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FashionShop.Infrastructure.Data
+{
+    public class FashionDbContext : DbContext
+    {
+        public FashionDbContext(DbContextOptions<FashionDbContext> options): base(options) { }
+
+        public DbSet<Product> Products { get; set; }
+        public DbSet<ProductBrand> ProductBrands { get; set; }
+        public DbSet<ProductType> ProductTypes { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        }
+    }
+}
---
+++ FashionShop.Infrastructure/Extensions/ApplicationServiceExtensions.cs
@@ -0,0 +1,20 @@
+﻿using FashionShop.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace FashionShop.Infrastructure.Extensions
+{
+    public static class ApplicationServiceExtensions
+    {
+        public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration config)
+        {
+            services.AddDbContext<FashionDbContext>(options =>
+            {
+                options.UseSqlServer(config.GetConnectionString("Default"));
+            });
+
+            return services;
+        }
+    }
+}
